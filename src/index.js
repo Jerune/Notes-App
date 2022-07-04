9d914e6c85dd44479b6a1beec9b180b981908f3b
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-07-04 11:10:10 +0200

    feat: Lazy initialize state from localStorage
    localStorage state initialisation happens once now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// @ts-nocheck
 import React from "react";
-import ReactDOM from "react-dom";
 import "./styles/main.css";
 import App from "./pages/App/App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
